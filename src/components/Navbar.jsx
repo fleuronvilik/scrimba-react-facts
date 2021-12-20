3031eb304e92dbfd1874e3cf6e9b4302f48b0c8b
Glitch (best-legendary-building) <none>
2021-12-20 20:41:30 +0000

    Start styling Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,10 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <h1>Navbar goes here</h1>
+    <nav className="navbar">
+      <img className="logo" src="https://cdn.glitch.me/50808bf5-d50c-4d94-81c5-33e926bdeaf3/react-icon-small.png?v=1640029961635"/>
+      <h3 className="title">ReactFacts</h3>
+      <h4 className="title-extra">React Course - Project 1</h4>
+    </nav>
   )
 }
